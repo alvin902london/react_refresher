8fe1628035187febf92ced2408dd4d6199c9c850
alvin902london <chun.alvinmok@gmail.com>
2022-05-15 21:37:47 +0100

    conditional text

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,17 +11,29 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
     return (
         <Card className="expenses">
-            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-            {props.items.map(expense => (
-                <ExpenseItem 
-                    key={expense.id}
-                    title={expense.title} 
-                    amount={expense.amount} 
-                    date={expense.date}
-                /> 
-            ))}
+            <ExpensesFilter
+                selected={filteredYear}
+                onChangeFilter={filterChangeHandler}
+            />
+            {filteredExpenses.length === 0 ? (
+                <p>No expenses found.</p>
+            ) : (
+                filteredExpenses.map(expense => (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))
+            )}
+
         </Card>
     );
 };
